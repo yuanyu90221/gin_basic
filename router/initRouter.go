e3239b31a582c5a88dfad59a6b3b8d4430d16373
jsonLiang <yuanyu90221@gmail.com>
2020-06-13 12:49:10 +0800

    update server

--- router/initRouter.go
+++ router/initRouter.go
@@ -1,7 +1,9 @@
 package router
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	md "github.com/yuanyu90221/gin_basic/middle"
@@ -9,6 +11,21 @@ import (
 
 func SetupRouter() *gin.Engine {
 	initRouter := gin.Default()
+	initRouter.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
 	initRouter.Use(md.Logger())
 	// get router
 	initRouter.GET("/", func(context *gin.Context) {
