c94c33f15e2f3f166df3f7fc9f693b8abbee2a16
jsonLiang <yuanyu90221@gmail.com>
2020-06-13 12:36:34 +0800

    add middle

--- router/initRouter.go
+++ router/initRouter.go
@@ -4,15 +4,19 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	md "github.com/yuanyu90221/gin_basic/middle"
 )
 
 func SetupRouter() *gin.Engine {
 	initRouter := gin.Default()
+	initRouter.Use(md.Logger())
 	// get router
 	initRouter.GET("/", func(context *gin.Context) {
 		// context.String(http.StatusOK, "hello gin")
+		middle := context.MustGet("middle").(string)
 		context.JSON(http.StatusOK, gin.H{
 			"message": "hello gin",
+			"middle":  middle,
 		})
 	})
 	return initRouter
